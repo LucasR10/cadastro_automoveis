792348681e4edf5edefa42722232ea3f91a6ce71
unknown <lucask@gmail.com>
2020-09-25 21:50:08 -0300

    commit inicial

---
+++ automoveis-api/src/main/java/br/com/itauunibanco/automoveis/api/AutomoveisApplication.java
@@ -0,0 +1,23 @@
+package br.com.itauunibanco.automoveis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class resposnsavel pela executa da aplicação
+ * 
+ * @author Lucas Carvalho
+ * @author https://www.linkedin.com/in/lucas-carvalho-793609134/
+ * @version 1.0
+ * @since 1.0 {@link AutomoveisApplication}
+ * 
+ */
+
+@SpringBootApplication
+public class AutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomoveisApplication.class, args);
+	}
+
+}
